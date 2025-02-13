ae658a855b904a6687f1d72426a8584998819373
Minsoo Kang <alstn38@naver.com>
2025-02-13 21:49:23 +0900

    chore: StringLiterals MainWeatherView button title 추가

---
+++ CapitalWeather/CapitalWeather/Resource/StringLiterals.swift
@@ -0,0 +1,15 @@
+//
+//  StringLiterals.swift
+//  CapitalWeather
+//
+//  Created by 강민수 on 2/13/25.
+//
+
+import Foundation
+
+enum StringLiterals {
+    
+    enum MainWeatherView {
+        static let moreInfoButtonTitle: String = "5일간 예보 보러가기"
+    }
+}
