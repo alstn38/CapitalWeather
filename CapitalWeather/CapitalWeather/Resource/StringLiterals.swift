4a88cf689535f8446016e37b898dc4bad848964d
Minsoo Kang <alstn38@naver.com>
2025-02-14 18:19:25 +0900

    chore: StringLiterals SearchView Text 추가

--- CapitalWeather/CapitalWeather/Resource/StringLiterals.swift
+++ CapitalWeather/CapitalWeather/Resource/StringLiterals.swift
@@ -9,7 +9,16 @@ import Foundation
 
 enum StringLiterals {
     
+    enum NavigationItem {
+        static let backButtonTitle: String = ""
+    }
+    
     enum MainWeatherView {
         static let moreInfoButtonTitle: String = "5일간 예보 보러가기"
+    }
+    
+    enum SearchView {
+        static let title: String = "도시 검색"
+        static let searchPlaceholder: String = "지금, 날씨가 궁금한 곳은?"
     }
 }
