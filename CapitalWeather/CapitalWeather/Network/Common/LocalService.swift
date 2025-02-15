a7921729434f3fd98be4e12c84e7cb3bfbfb0fe2
Minsoo Kang <alstn38@naver.com>
2025-02-15 20:43:11 +0900

    feat: LocalService Model 구현

---
+++ CapitalWeather/CapitalWeather/Network/Common/LocalService.swift
@@ -0,0 +1,37 @@
+//
+//  LocalService.swift
+//  CapitalWeather
+//
+//  Created by 강민수 on 2/15/25.
+//
+
+import Foundation
+
+final class LocalService {
+    
+    static let shared = LocalService()
+    
+    private init() { }
+    
+    func loadJsonData<T: Decodable>(
+        fileName: String,
+        responseType: T.Type,
+        completionHandler: @escaping (Result<T, LocalServiceError>) -> Void
+    ) {
+        let extensionType = "json"
+        guard let fileLocation = Bundle.main.url(
+            forResource: fileName,
+            withExtension: extensionType
+        ) else {
+            completionHandler(.failure(.invalidPath))
+        }
+        
+        do {
+            let data = try Data(contentsOf: fileLocation)
+            let decodedData = try JSONDecoder().decode(T.self, from: data)
+            completionHandler(.success(decodedData))
+        } catch {
+            completionHandler(.failure(.decodeError))
+        }
+    }
+}
