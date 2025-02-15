1ba34be725fbc4b4f9820d533653396b30e408aa
Minsoo Kang <alstn38@naver.com>
2025-02-15 20:42:35 +0900

    feat: LocalServiceError type 정의

---
+++ CapitalWeather/CapitalWeather/Network/Common/LocalServiceError.swift
@@ -0,0 +1,25 @@
+//
+//  LocalServiceError.swift
+//  CapitalWeather
+//
+//  Created by 강민수 on 2/15/25.
+//
+
+import Foundation
+
+enum LocalServiceError: Error {
+    case invalidPath
+    case decodeError
+}
+
+extension LocalServiceError: LocalizedError {
+    
+    var errorDescription: String? {
+        switch self {
+        case .invalidPath:
+            return "invalidPath"
+        case .decodeError:
+            return "decoded Error"
+        }
+    }
+}
