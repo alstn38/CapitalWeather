e58a7877e8afe944a1d6aefa1b1262bb1bb5bf89
Minsoo Kang <alstn38@naver.com>
2025-02-16 19:10:10 +0900

    chore: UnsplashSearchPhotoDTO toEntity method 생성

--- CapitalWeather/CapitalWeather/Network/DTO/UnsplashSearchPhotoDTO.swift
+++ CapitalWeather/CapitalWeather/Network/DTO/UnsplashSearchPhotoDTO.swift
@@ -9,6 +9,14 @@ import Foundation
 
 struct UnsplashSearchPhotoDTO: Decodable {
     let results: [Picture]
+    
+    func toEntity() -> SearchPhotoEntity {
+        return SearchPhotoEntity(
+            photoLink: self.results.first?.urls.smallSizeLink ?? "",
+            width: self.results.first?.width ?? 0,
+            height: self.results.first?.height ?? 0
+        )
+    }
 }
 
 struct Picture: Decodable {
