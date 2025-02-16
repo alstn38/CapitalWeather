22af3ef5d7fb966b7736e40e5b8005d2ccfbd66d
Minsoo Kang <alstn38@naver.com>
2025-02-16 15:23:51 +0900

    feat: CityInfoEntity 정의

---
+++ CapitalWeather/CapitalWeather/Model/Entity/CityInfoEntity.swift
@@ -0,0 +1,14 @@
+//
+//  CityInfoEntity.swift
+//  CapitalWeather
+//
+//  Created by 강민수 on 2/16/25.
+//
+
+import Foundation
+
+struct CityInfoEntity {
+    let cityName: String
+    let countryName: String
+    let id: Int
+}
