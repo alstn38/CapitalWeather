19244474193fdc37ad8d43578bb48303a037ecbb
Minsoo Kang <alstn38@naver.com>
2025-02-16 18:20:45 +0900

    chore: Secret OpenWeatherIconURL method 추가

--- CapitalWeather/CapitalWeather/Resource/Secret.swift
+++ CapitalWeather/CapitalWeather/Resource/Secret.swift
@@ -13,34 +13,39 @@ enum Secret {
         guard let urlString = Bundle.main.infoDictionary?["UNSPLASH_BASE_URL"] as? String else {
             fatalError("UNSPLASH_BASE_URL ERROR")
         }
+        
         return urlString
     }()
     
     static let unsplashClientID: String = {
-        guard let accessToken = Bundle.main.infoDictionary?["UNSPLASH_CLIENT_ID"] as? String else {
+        guard let clientID = Bundle.main.infoDictionary?["UNSPLASH_CLIENT_ID"] as? String else {
             fatalError("UNSPLASH_CLIENT_ID ERROR")
         }
-        return accessToken
+        
+        return clientID
     }()
     
     static let openWeatherBaseURL: String = {
-        guard let accessToken = Bundle.main.infoDictionary?["OPENWEATHER_BASE_URL"] as? String else {
+        guard let urlString = Bundle.main.infoDictionary?["OPENWEATHER_BASE_URL"] as? String else {
             fatalError("OPENWEATHER_BASE_URL ERROR")
         }
-        return accessToken
+        
+        return urlString
     }()
     
     static let openWeatherAppID: String = {
-        guard let accessToken = Bundle.main.infoDictionary?["OPENWEATHER_APP_ID"] as? String else {
+        guard let appID = Bundle.main.infoDictionary?["OPENWEATHER_APP_ID"] as? String else {
             fatalError("OPENWEATHER_APP_ID ERROR")
         }
-        return accessToken
+        
+        return appID
     }()
     
-    static let openWeatherIconURL: String = {
-        guard let accessToken = Bundle.main.infoDictionary?["OPENWEATHER_ICON_URL"] as? String else {
+    static func openWeatherIconURL(with icon: String) -> String {
+        guard let urlString = Bundle.main.infoDictionary?["OPENWEATHER_ICON_URL"] as? String else {
             fatalError("OPENWEATHER_ICON_URL ERROR")
         }
-        return accessToken
-    }()
+        
+        return urlString + "/img/wn/\(icon)@2x.png"
+    }
 }
