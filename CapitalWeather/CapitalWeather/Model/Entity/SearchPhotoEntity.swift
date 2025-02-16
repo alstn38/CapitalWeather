52011c78f169bc4efcff9213201d39c0d8b2fa0f
Minsoo Kang <alstn38@naver.com>
2025-02-16 19:09:41 +0900

    feat: SearchPhotoEntity Model 정의

---
+++ CapitalWeather/CapitalWeather/Model/Entity/SearchPhotoEntity.swift
@@ -0,0 +1,14 @@
+//
+//  SearchPhotoEntity.swift
+//  CapitalWeather
+//
+//  Created by 강민수 on 2/16/25.
+//
+
+import Foundation
+
+struct SearchPhotoEntity {
+    let photoLink: String
+    let width: Int
+    let height: Int
+}
