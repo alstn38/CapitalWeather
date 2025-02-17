cb729d982d693710dc9d5c9d1cd08f2d11f5ec70
Minsoo Kang <alstn38@naver.com>
2025-02-17 19:19:42 +0900

    feat: SearchWeatehrEntity Model 정의

---
+++ CapitalWeather/CapitalWeather/Model/Entity/SearchWeatherEntity.swift
@@ -0,0 +1,13 @@
+//
+//  SearchWeatherEntity.swift
+//  CapitalWeather
+//
+//  Created by 강민수 on 2/17/25.
+//
+
+import Foundation
+
+struct SearchWeatherEntity {
+    let cityInfoEntity: CityInfoEntity
+    let currentWeatherEntity: CurrentWeatherEntity
+}
